2bb06f1474593c635bc09ab565088f2d23d26358
Yogesh Chhabra <chhabra99yogesh@gmail.com>
2019-01-12 12:55:17 +0530

    created the basic layout of the homepage

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,6 @@ void main() => runApp(MyApp());
 (251,153,228)(246,172,226)(255,191,238)(252,215,246)
 */
 
-
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -17,33 +15,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        fontFamily: 'Karla',
+        primarySwatch: Colors.deepPurple,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,68 +34,173 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
+ // var news = ()=> ;
+
+  final pageController = new PageController();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+     /* bottomNavigationBar: BottomAppBar(
+        child: new Container(
+          decoration: BoxDecoration(
+            color: Colors.lightBlue[100],
+          ),
+        ),
+      ),*/
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: new Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 5,
+                child: Container(),
+              ),
+              Expanded(
+                  flex: 29,
+                  child: new Container(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new Expanded(
+                            flex: 35,
+                            child: new Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.lightBlue[100],
+                                  shape: BoxShape.circle),
+                            )),
+                        new Expanded(flex: 5, child: new Container()),
+                        new Expanded(
+                            flex: 60,
+                            child: new Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.lightBlue[100],
+                                  borderRadius: BorderRadius.circular(20)),
+                              padding: EdgeInsets.all(19),
+                              child: new Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Expanded(
+                                    flex: 6,
+                                    child: Text(
+                                      "Yogesh Chhabra",
+                                      style: TextStyle(
+                                          fontFamily: 'Karla',
+                                          fontSize: 23,
+                                          color: Colors.black),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Text(
+                                      "Indian Institute of Technlogy Ropar",
+                                      style: TextStyle(
+                                          fontFamily: 'Karla',
+                                          fontSize: 15,
+                                          color: Colors.black,
+                                          fontStyle: FontStyle.italic),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: new Container(),
+                                  ),
+                                  Expanded(
+                                      flex: 5,
+                                      child: new Container(
+                                        child:PageView(
+                                          
+                                          children:List.generate(4, (index){
+                                            var col = Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                                Text(
+                                                  "$index Club Name",
+                                                  
+                                                  style: TextStyle(
+                                                      fontFamily: 'Karla',
+                                                      fontSize: 15,
+                                                      color: Colors.black),
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.all(1),
+                                                  child: new Container(),
+                                                ),
+                                                new Row(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: <Widget>[
+                                                    Text(
+                                                      "$index Event Name",
+                                                      style: TextStyle(
+                                                          fontFamily: 'Karla',
+                                                          fontSize: 13,
+                                                          color: Colors.black),
+                                                    ),
+                                                    Padding(
+                                                      padding:
+                                                          EdgeInsets.all(6),
+                                                      child: new Container(),
+                                                    ),
+                                                    Text(
+                                                      "$index Event Time",
+                                                      style: TextStyle(
+                                                          fontFamily: 'Karla',
+                                                          fontSize: 13,
+                                                          color: Colors.black),
+                                                    ),
+                                                  ],
+                                                )
+                                              ]);
+                                              return Container(child:col);
+                                          }),
+                                          controller: pageController,
+                                          scrollDirection: Axis.horizontal,
+                                    )
+                                  )),
+                                ],
+                              ),
+                            )),
+                      ],
+                    ),
+                  )),
+              Expanded(
+                flex: 5,
+                child: Container(),
+              ),
+              Expanded(
+                  flex: 60,
+                  child: new Container(
+                      //color: Colors.red,
+                      padding: EdgeInsets.all(10),
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        children: List.generate(10, (index) {
+                          return Container(
+                              padding: EdgeInsets.all(8),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Colors.purple[100],
+                                ),
+                                child: Center(
+                                    child: Text(
+                                  "Item $index",
+                                  style: TextStyle(
+                                      fontFamily: 'Karla', fontSize: 15),
+                                )),
+                              ));
+                        }),
+                      )))
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
